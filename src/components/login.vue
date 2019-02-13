a23c447c0283176ed918fe1fdd5e0de5bd21b5b7
LMJbestgirl <1787643948@qq.com>
2019-02-13 22:17:46 +0800

    登录页面

---
+++ src/components/login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <el-form label-position="top" label-width="80px" :model="formData">
+    <h2>用户登录</h2>
+      <el-form-item label="邮箱">
+        <el-input v-model="formData.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formData.passworld"></el-input>
+      </el-form-item>
+       <el-button type="primary" plain>主要按钮</el-button>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      formData: {
+        username: '',
+        passworld: ''
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
