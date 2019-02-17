5e6470efb87c694137b4427ef1b6e703a11d5361
LMJbestgirl <1787643948@qq.com>
2019-02-17 18:30:25 +0800

    权限列表

--- src/components/rigths.vue
+++ src/components/rigths.vue
@@ -1,6 +1,11 @@
 <template>
   <el-card>
     <CusBread level1="权限管理" level2="权限列表"></CusBread>
+    <el-table :data="list" style="width: 100%">
+      <el-table-column prop="authName" label="日期" width="180"></el-table-column>
+      <el-table-column prop="id" label="姓名" width="180"></el-table-column>
+      <el-table-column prop="level" label="地址"></el-table-column>
+    </el-table>
   </el-card>
 </template>
 
@@ -20,6 +25,7 @@ export default {
       this.$http.defaults.headers.common["Authorization"] = AUTH_TOKEN;
       const res = await this.$http.get("rights/list");
       const { data } = res.data;
+      console.log(data);
       this.list = data;
     }
   }
