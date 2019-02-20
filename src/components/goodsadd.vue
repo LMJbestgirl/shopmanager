7e22ef00d168635a54bebaf03d7bc67e61b29345
LMJbestgirl <1787643948@qq.com>
2019-02-20 17:27:08 +0800

    添加商品完成

--- src/components/goodsadd.vue
+++ src/components/goodsadd.vue
@@ -7,7 +7,7 @@
       <el-step title="基本信息"></el-step>
       <el-step title="商品参数"></el-step>
       <el-step title="商品属性"></el-step>
-      <el-step title="商品图片"></el-step>
+      <el-step title="商品图g  片"></el-step>
       <el-step title="商品内容"></el-step>
     </el-steps>
     <!-- tab切换 -->
@@ -44,16 +44,45 @@
             </el-checkbox-group>
           </el-form-item>
         </el-tab-pane>
-        <el-tab-pane name="3" label="商品属性">3</el-tab-pane>
-        <el-tab-pane name="4" label="商品图片">4</el-tab-pane>
-        <el-tab-pane name="5" label="商品内容">5</el-tab-pane>
+        <el-tab-pane name="3" label="商品属性">
+          <el-form-item :label="item1.attr_name" v-for="(item1) in arrStatic" :key="item1.attr_id">
+            <el-input v-model="item1.attr_vals"></el-input>
+          </el-form-item>
+        </el-tab-pane>
+        <el-tab-pane name="4" label="商品图片">
+          <el-form-item>
+            <el-upload
+              class="upload-demo"
+              action="http://localhost:8888/api/private/v1/upload"
+              :headers="headers"
+              :on-remove="handleRemove"
+              :on-success="handleSuccess"
+              list-type="picture"
+            >
+              <el-button size="small" type="primary">点击上传</el-button>
+            </el-upload>
+          </el-form-item>
+        </el-tab-pane>
+        <el-tab-pane name="5" label="商品内容">
+          <el-form-item>
+            <el-button @click="addGoods()">添加商品</el-button>
+            <quill-editor class="quill" v-model="form.goods_introduce"></quill-editor>
+          </el-form-item>
+        </el-tab-pane>
       </el-tabs>
     </el-form>
   </el-card>
 </template>
 
 <script>
+import "quill/dist/quill.core.css";
+import "quill/dist/quill.snow.css";
+import "quill/dist/quill.bubble.css";
+import { quillEditor } from "vue-quill-editor";
 export default {
+  components: {
+    quillEditor
+  },
   data() {
     return {
       active: "1",
@@ -64,48 +93,119 @@ export default {
         goods_number: "",
         goods_weight: "",
         goods_introduce: "",
-        pics: "",
-        attrs: ""
+        pics: [],
+        attrs: []
       },
       arrDy: [],
-      selectedOptions: [1, 3, 6],
+      selectedOptions: [g1],
       options: [],
       defaultProp: {
         label: "cat_name",
         value: "cat_id",
         children: "children"
       },
-      checkList: []
+      checkList: [],
+      arrStatic: [],
+      headers: {
+        Authorization: localStorage.getItem("token")
+      }
     };
   },
   created() {
     this.getGoodsCate();
   },
   methods: {
-    async changeTab() {
-      const res = await this.$http.get(
-        `categories/${this.selectedOptions[2]}/attributes?sel=many`
-      );
+    async addGoods() {
+      this.form.goods_cat = this.selectedOptions.join(",");
+      const arr1 = this.arrDy.map(item => {
+        return { attr_id: item.attr_id, attr_value: item.attr_vals };
+      });
+      const arr2 = this.arrStatic.map(item => {
+        return { attr_id: item.attr_id, attr_value: item.attr_vals };
+      });
+      const arr = [...arr1, ...arr2];
+      this.form.attrs = arr;
+      console.log(this.form.attrs);
+      const res = await this.$http.post(`goods`, this.form);
+      console.log(res);
       const {
-        meta: { msg, status },
-        data
-      } = res.data;
-      if (status === 200) {
-        this.arrDy = data;
-        this.arrDy.forEach(item => {
-          item.attr_vals =
-            item.attr_vals.trim().length === 0
-              ? []
-              : item.attr_vals.trim().split(",");
+        data: {
+          meta: { msg, status },
+          data
+        }
+      } = res;
+      if (status === 201) {
+        this.$message.success(msg);
+        this.$router.push({
+          name: "goodlist"
         });
       }
+    },
 
-      console.log(this.arrDy);
+    handleSuccess(response) {
+      console.log(response);
+
+      this.form.pics.push = {
+        pic: response.data.tmp_path
+      };
+    },
+    handleRemove(file) {
+      console.log(file);
+      const Index = this.form.pics.findIndex(item => {
+        return item.pic === file.response.data.tmp_path;
+      });
+      this.form.pics.splice(Index, 1);
+    },
+    async changeTab() {
+      if (this.active === "2" || this.active === "3") {
+        if (this.selectedOptions.length !== 3) {
+          this.$message.error("请先选择三级分类!");
+          if (this.arrDy === "2") {
+            this.arrDy = [];
+          } else {
+            this.arrStatic = [];
+          }
+          return;
+        }
+
+        if (this.active === "3") {
+          const res1 = await this.$http.get(
+            `categories/${this.selectedOptions[2]}/attributes?sel=only`
+          );
+
+          const {
+            meta: { msg, status },
+            data
+          } = res1.data;
+          if (status === 200) {
+            this.arrStatic = data;
+          }
+        }
+
+        if (this.active === "2") {
+          const res = await this.$http.get(
+            `categories/${this.selectedOptions[2]}/attributes?sel=many`
+          );
+          const {
+            meta: { msg, status },
+            data
+          } = res.data;
+          console.log(data);
+          if (status === 200) {
+            this.arrDy = data;
+            this.arrDy.forEach(item => {
+              item.attr_vals =
+                item.attr_vals.trim().length === 0
+                  ? []
+                  : item.attr_vals.trim().split(",");
+            });
+          }
+        }
+      }
     },
     handleChange() {},
     async getGoodsCate() {
       const res = await this.$http.get(`categories?type=3`);
-      console.log(res);
       const {
         meta: { msg, status },
         data
@@ -130,5 +230,9 @@ export default {
   overflow: auto;
   margin-top: 20px;
   height: 350px;
+}
+.ql-editor,
+.ql-blank {
+  min-height: 200px;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -39,7 +39,7 @@ const router = new Router({
       },
       {
         path: '/goods',
-        name: 'goods',
+        name: 'goodlist',
         component: Goodlist
       },
       {
